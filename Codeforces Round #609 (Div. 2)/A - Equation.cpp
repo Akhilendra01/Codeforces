19a4301641fa46f8f41a76fa7552b2cb05d8cdc1
Ahmed Elkalaf <kalaf1997@yahoo.com>
2019-12-24 13:44:34 +0200

    Create A - Equation.cpp

---
+++ Codeforces Round #609 (Div. 2)/A - Equation.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int a,b,n;
+ 
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //--------------------------
+    scanf("%d",&n);
+    printf("%d %d\n",9*n,8*n);
+ 
+    return 0;
+}
